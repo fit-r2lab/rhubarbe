1119c35f755825a9de31e1d3204b682985f6024b
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-12-17 16:53:56 +0100

    for 3.14, get_event_loop was raising an exception, use Runner instead

--- rhubarbe/monitor/loop.py
+++ rhubarbe/monitor/loop.py
@@ -14,31 +14,27 @@ class MonitorLoop:
 
     def run(self, async_main):
 
-        # using new_event_loop() instead here results in a runtime
-        # error with apparently 2 separate event loops active
-        loop = asyncio.get_event_loop()
+        async def async_main_wrapper():
+            loop = asyncio.get_running_loop()
 
-        def exiting(signame):
-            logger.info(f"rhubarbe-{self.message}: ¬ received signal {signame} - exiting")
-            loop.stop()
-            exit(1)
+            def exiting(signame):
+                logger.info(f"rhubarbe-{self.message}: ¬ received signal {signame} - exiting")
+                loop.stop()
+                exit(1)
 
-        for signame in ('SIGHUP', 'SIGQUIT', 'SIGINT', 'SIGTERM'):
-            loop.add_signal_handler(getattr(signal, signame),
-                                    functools.partial(exiting, signame))
+            for signame in ('SIGHUP', 'SIGQUIT', 'SIGINT', 'SIGTERM'):
+                loop.add_signal_handler(getattr(signal, signame),
+                                        functools.partial(exiting, signame))
 
-        try:
-            task = asyncio.ensure_future(async_main)
-            loop.run_until_complete(task)
-            return 0
-        except KeyboardInterrupt:
-            logger.info(f"rhubarbe-{self.message} : keyboard interrupt - exiting")
-            task.cancel()
-            loop.run_forever()
-            task.exception()
-            return 1
-        except asyncio.TimeoutError:
-            logger.info(f"rhubarbe-{self.message} : asyncio timeout expired")
-            return 1
-        finally:
-            loop.close()
+            try:
+                await async_main
+                return 0
+            except KeyboardInterrupt:
+                logger.info(f"rhubarbe-{self.message} : keyboard interrupt - exiting")
+                return 1
+            except asyncio.TimeoutError:
+                logger.info(f"rhubarbe-{self.message} : asyncio timeout expired")
+                return 1
+
+        with asyncio.Runner() as runner:
+            return runner.run(async_main_wrapper())
